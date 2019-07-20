a34ab57b22dc454e9c97d9b8f4e103b29dc053d9
yiyexingyu <2958029539@qq.com>
2019-07-20 23:46:39 +0800

    删除一些多余的代码，添加了视图管理和首选项弹出窗口

---
+++ WindowStyle/StyleSheet.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/20 23:42
+# @Author  : 何盛信
+# @Email   : 2958029539@qq.com
+# @File    : StyleSheet.py
+# @Project : LSRMSingle
+# @Software: PyCharm
+
+""" 描述 """
